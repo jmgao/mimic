df99a83971b5f0bf8301e29ef732ffa1c1afa813
Josh Gao <josh@insolit.us>
2016-02-28 19:36:29 -0800

    Split out logging macros.

--- src/aoa.h
+++ src/aoa.h
@@ -7,12 +7,7 @@
 
 #include <libusb.h>
 
-#define _log(fmt, prefix, ...) fprintf(stderr, prefix fmt "\n", ##__VA_ARGS__)
-#define log(fmt, ...) _log(fmt, "", ##__VA_ARGS__)
-#define debug(fmt, ...) _log(fmt, "debug: ", ##__VA_ARGS__)
-#define info(fmt, ...) _log(fmt, "info: ", ##__VA_ARGS__)
-#define warn(fmt, ...) _log(fmt, "warning: ", ##__VA_ARGS__)
-#define error(fmt, ...) _log(fmt, "error: ", ##__VA_ARGS__)
+#include "log.h"
 
 enum class AOAMode {
   accessory = 1 << 0,
---
+++ src/log.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdio.h>
+
+#define _log(fmt, prefix, ...) fprintf(stderr, prefix fmt "\n", ##__VA_ARGS__)
+#define log(fmt, ...) _log(fmt, "", ##__VA_ARGS__)
+#define debug(fmt, ...) _log(fmt, "debug: ", ##__VA_ARGS__)
+#define info(fmt, ...) _log(fmt, "info: ", ##__VA_ARGS__)
+#define warn(fmt, ...) _log(fmt, "warning: ", ##__VA_ARGS__)
+#define error(fmt, ...) _log(fmt, "error: ", ##__VA_ARGS__)
